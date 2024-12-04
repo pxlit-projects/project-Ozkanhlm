c853632d8a7a8d77bbd9461904f3026f0dfe8058
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-12-04 18:48:01 +0100

    add discovery service

--- backend-java/ContentbeheerSevices/review-service/src/main/java/be/pxl/services/ReviewServiceApplication.java
+++ backend-java/ContentbeheerSevices/review-service/src/main/java/be/pxl/services/ReviewServiceApplication.java
@@ -2,11 +2,13 @@ package be.pxl.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * ReviewServiceApplication
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewServiceApplication
 {
     public static void main( String[] args )
