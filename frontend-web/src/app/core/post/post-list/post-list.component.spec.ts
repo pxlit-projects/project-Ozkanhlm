0bb7f4ec07751d7399f994a108f307ddbe817889
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-12-29 21:55:54 +0100

    frontend tests postListComponent

--- frontend-web/src/app/core/post/post-list/post-list.component.spec.ts
+++ frontend-web/src/app/core/post/post-list/post-list.component.spec.ts
@@ -1,23 +1,94 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { PostListComponent } from './post-list.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { PostService } from '../../../shared/services/post.service';
+import { Post } from '../../../shared/models/post.model';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { of } from 'rxjs';
 
-// import { PostListComponent } from './post-list.component';
+describe('PostListComponent', () => {
+  let component: PostListComponent;
+  let fixture: ComponentFixture<PostListComponent>;
+  let postServiceSpy: jasmine.SpyObj<PostService>;
 
-// describe('PostListComponent', () => {
-//   let component: PostListComponent;
-//   let fixture: ComponentFixture<PostListComponent>;
+  const mockPosts: Post[] = [
+    {
+      id: 1,
+      title: 'Post 1',
+      content: 'Content 1',
+      author: 'Ozkan',
+      category: 'EVENTS',
+      status: 'PUBLISH',
+      createdDate: '2023-12-29',
+      picture: '',
+      reviews: [],
+    },
+    {
+      id: 2,
+      title: 'Post 2',
+      content: 'Content 2',
+      author: 'Halim',
+      category: 'EVENTS',
+      status: 'PUBLISH',
+      createdDate: '2023-12-29',
+      picture: '',
+      reviews: [],
+    },
+    {
+      id: 3,
+      title: 'Post 3',
+      content: 'Content 3',
+      author: 'Random',
+      category: 'EVENTS',
+      status: 'PUBLISH',
+      createdDate: '2023-12-29',
+      picture: '',
+      reviews: [],
+    },
+  ];
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [PostListComponent]
-//     })
-//     .compileComponents();
+  beforeEach(async () => {
+    const postServiceSpyObj = jasmine.createSpyObj('PostService', ['getPosts']);
 
-//     fixture = TestBed.createComponent(PostListComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    await TestBed.configureTestingModule({
+      imports: [PostListComponent],
+      providers: [
+        provideNativeDateAdapter(),
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        { provide: PostService, useValue: postServiceSpyObj },
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    postServiceSpy = TestBed.inject(PostService) as jasmine.SpyObj<PostService>;
+    fixture = TestBed.createComponent(PostListComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should fetch posts', fakeAsync(() => {
+    postServiceSpy.getPosts.and.returnValue(of(mockPosts));
+    component.ngOnInit();
+
+    tick();
+
+    expect(postServiceSpy.getPosts).toHaveBeenCalled();
+    expect(component.allPosts.length).toBe(3);
+    expect(component.posts.length).toBe(3);
+    console.log(component.posts);
+    expect(component.posts[0].id).toBe(3);
+    expect(component.posts[1].id).toBe(2);
+    expect(component.posts[2].id).toBe(1);
+  }));
+});
