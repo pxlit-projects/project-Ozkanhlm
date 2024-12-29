703fcf9723058200a813b589a67217a59e5eee05
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-12-29 22:31:43 +0100

    frontend tests workbanchComponent

--- frontend-web/src/app/core/post/workbanch/workbanch.component.spec.ts
+++ frontend-web/src/app/core/post/workbanch/workbanch.component.spec.ts
@@ -1,27 +1,48 @@
-// /* tslint:disable:no-unused-variable */
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { By } from '@angular/platform-browser';
-// import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WorkbanchComponent } from './workbanch.component';
+import { of } from 'rxjs';
+import { PostService } from '../../../shared/services/post.service';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// import { WorkbanchComponent } from './workbanch.component';
+describe('WorkbanchComponent', () => {
+  let component: WorkbanchComponent;
+  let fixture: ComponentFixture<WorkbanchComponent>;
 
-// describe('WorkbanchComponent', () => {
-//   let component: WorkbanchComponent;
-//   let fixture: ComponentFixture<WorkbanchComponent>;
+  const postServiceMock = {
+    getPosts: jasmine.createSpy().and.returnValue(of([])),
+  };
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [WorkbanchComponent],
-//     }).compileComponents();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [WorkbanchComponent, BrowserAnimationsModule],
+      providers: [
+        { provide: PostService, useValue: postServiceMock },
+        provideNativeDateAdapter(),
+      ],
+    }).compileComponents();
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(WorkbanchComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    fixture = TestBed.createComponent(WorkbanchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should fetch posts on init', () => {
+    postServiceMock.getPosts.and.returnValue(
+      of([
+        { id: 1, status: 'CONCEPT' },
+        { id: 2, status: 'PENDING' },
+      ])
+    );
+
+    component.ngOnInit();
+
+    expect(postServiceMock.getPosts).toHaveBeenCalled();
+    expect(component.conceptPosts.length).toBe(1);
+    expect(component.pendingPosts.length).toBe(1);
+  });
+});
