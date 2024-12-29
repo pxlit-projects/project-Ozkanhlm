48c323eb52cc5a72c5662d1930506bb1010d7b3f
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-12-29 17:33:09 +0100

    frontend tests addPostComponent

--- frontend-web/src/app/core/post/add-post/add-post.component.spec.ts
+++ frontend-web/src/app/core/post/add-post/add-post.component.spec.ts
@@ -1,22 +1,82 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AddPostComponent } from './add-post.component';
+import { PostService } from '../../../shared/services/post.service';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Post } from '../../../shared/models/post.model';
 
-// import { AddPostComponent } from './add-post.component';
+describe('AddPostComponent', () => {
+  let component: AddPostComponent;
+  let fixture: ComponentFixture<AddPostComponent>;
+  let postService: PostService;
 
-// describe('AddPostComponent', () => {
-//   let component: AddPostComponent;
-//   let fixture: ComponentFixture<AddPostComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AddPostComponent, ReactiveFormsModule, BrowserAnimationsModule],
+      providers: [
+        {
+          provide: PostService,
+          useValue: {
+            getCategories: jasmine
+              .createSpy()
+              .and.returnValue(
+                of(['NEWS', 'UPDATES', 'EVENTS', 'ANNOUNCEMENTS'])
+              ),
+            getStatuses: jasmine
+              .createSpy()
+              .and.returnValue(of(['PUBLISH', 'CONCEPT', 'PENDING'])),
+            addPost: jasmine.createSpy().and.returnValue(of({})),
+          },
+        },
+      ],
+    }).compileComponents();
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [AddPostComponent],
-//     }).compileComponents();
+    fixture = TestBed.createComponent(AddPostComponent);
+    component = fixture.componentInstance;
+    postService = TestBed.inject(PostService);
+    fixture.detectChanges();
+  });
 
-//     fixture = TestBed.createComponent(AddPostComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should submit the form and call addPost method', (done) => {
+    const post: Post = {
+      id: 1,
+      title: 'Test Title',
+      picture: 'test-picture-url.jpg',
+      content: 'Test Content',
+      author: 'Test Author',
+      status: 'PUBLISH',
+      category: 'EVENTS',
+      createdDate: '2023-12-29',
+    };
+
+    component.postForm.controls['title'].setValue(post.title);
+    component.postForm.controls['content'].setValue(post.content);
+    component.postForm.controls['picture'].setValue(post.picture);
+    component.postForm.controls['author'].setValue(post.author);
+    component.postForm.controls['category'].setValue(post.category);
+    component.postForm.controls['status'].setValue(post.status);
+
+    component.onSubmit();
+
+    fixture.whenStable().then(() => {
+      const expectedPost = {
+        title: 'Test Title',
+        picture: 'test-picture-url.jpg',
+        content: 'Test Content',
+        author: 'Test Author',
+        status: 'PUBLISH',
+        category: 'EVENTS',
+      };
+
+      expect(postService.addPost).toHaveBeenCalledWith(
+        jasmine.objectContaining(expectedPost)
+      );
+      done();
+    });
+  });
+});
