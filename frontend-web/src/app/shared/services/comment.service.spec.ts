f896b09de9a4e13dd857e65abb9bfa937c34ff24
Ozkanhlm <Ozkanhalim3600@gmail.com>
2024-12-29 16:47:40 +0100

    frontend tests commentService

--- frontend-web/src/app/shared/services/comment.service.spec.ts
+++ frontend-web/src/app/shared/services/comment.service.spec.ts
@@ -1,16 +1,65 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { CommentService } from './comment.service';
+import { Comment } from '../models/comment.model';
 
-// import { CommentService } from './comment.service';
+describe('CommentService', () => {
+  let service: CommentService;
+  let httpTestingController: HttpTestingController;
 
-// describe('CommentService', () => {
-//   let service: CommentService;
+  const mockComment: Comment = {
+    id: 1,
+    user: 'gebruiker',
+    comment: 'This is a test comment.',
+    postId: 101,
+  };
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(CommentService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        CommentService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+    service = TestBed.inject(CommentService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  afterEach(() => {
+    // Chek afhandeling van requesten
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add a comment', () => {
+    service.addComment(mockComment).subscribe((comment) => {
+      expect(comment).toEqual(mockComment);
+    });
+
+    const req = httpTestingController.expectOne(service.api);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(mockComment);
+
+    req.flush(mockComment);
+  });
+
+  it('should delete a comment', () => {
+    service.deleteComment(mockComment.id).subscribe((response) => {
+      expect(response).toBeNull();
+    });
+
+    const req = httpTestingController.expectOne(
+      `${service.api}/${mockComment.id}`
+    );
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
+});
